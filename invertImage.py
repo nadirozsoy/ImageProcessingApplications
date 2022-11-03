a93a346cb45511f622130677da57846dcab06df4
Nadir Özsoy <thisisnadirozsoy@gmail.com>
2022-11-03 10:53:40 +0300

    Master 2 | invert-image

---
+++ invertImage.py
@@ -0,0 +1,24 @@
+#Invert Image
+
+import cv2
+import numpy as np
+
+#image reading
+image = cv2.imread("car.jpeg",0)
+cv2.imshow("original",image)
+
+#manuel invert
+[h,w] = image.shape
+new_image = np.zeros([h,w], dtype=np.uint8)
+for i in range(h):
+    for j in range(w):
+        new_image[i,j] = 255 - image[i,j]
+print(image[0,0])
+cv2.imshow("Manuel_inverted",new_image)
+
+
+#invert image reverse
+inverted = np.invert(image)
+cv2.imshow("inverted",inverted)
+negimage = 255 - image
+cv2.waitKey()
